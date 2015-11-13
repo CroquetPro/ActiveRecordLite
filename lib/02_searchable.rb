3730483be6ae41b3e9263a5c463c70ee62493772
Ben Rothman <croquetpro@gmail.com>
2015-11-13 14:54:05 -0800

    Finished Searchable

--- lib/02_searchable.rb
+++ lib/02_searchable.rb
@@ -1,12 +1,23 @@
 require_relative 'db_connection'
 require_relative '01_sql_object'
+require 'byebug'
 
 module Searchable
   def where(params)
-    # ...
+    table = self.table_name
+    where_line = params.keys.map { |key| "#{key} = ?" }.join(" AND ")
+    results = DBConnection.execute(<<-SQL, *params.values)
+        SELECT *
+        FROM #{table}
+        WHERE #{where_line}
+      SQL
+
+    parse_all(results)
+
   end
 end
 
 class SQLObject
+  extend Searchable
   # Mixin Searchable here...
 end
